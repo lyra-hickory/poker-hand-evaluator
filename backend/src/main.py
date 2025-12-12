e135815a03ab409df3b28dd8d7500fa2cea4c087
Abigail Lucas <abigaillucas@protonmail.com>
2025-12-12 13:50:32 +0200

    Add cards length check to /eval-hand/

--- backend/src/main.py
+++ backend/src/main.py
@@ -13,9 +13,15 @@ async def root():
 
 @app.post("/eval-hand/")
 async def evaluate(cards: list[Card], response: Response):
-    # Validate the card value input
+    # Validate the cards input
+    # Valid amount of cards?
+    if len(cards) != 5:
+        response.status_code = status.HTTP_400_BAD_REQUEST
+        return f'Invalid amount of cards given, 5 cards are expected.'
+    # Valid values?
     for c in cards:
         if c.value_as_int is None:
             response.status_code = status.HTTP_400_BAD_REQUEST
             return f'Invalid value given for {c}. Values must conform to {c.valid_value_keys}...'
+    # Evaluate our hand and return as response
     return evaluate_hand(cards)
