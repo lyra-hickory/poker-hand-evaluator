e03788a6b2a00aaf1de575826b670e829968f9e6
Abigail Lucas <abigaillucas@protonmail.com>
2025-12-15 16:58:14 +0200

    tests/addCardToHand: should add

--- web/src/app/components/card-form/card-form.spec.ts
+++ web/src/app/components/card-form/card-form.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CardForm } from './card-form';
+import {Card} from '../../interfaces/handOfCards';
+import {expect} from 'vitest';
 
 describe('CardForm', () => {
   let component: CardForm;
@@ -19,5 +21,27 @@ describe('CardForm', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('addCardToHand', () => {
+    it('should add a card to the hand when button is clicked', () => {
+      // assemble
+      const cardToAdd:Card = {
+        value: 'king',
+        suit: 'd'
+      };
+      component.inputCard.set(cardToAdd);
+      const button  = fixture.nativeElement.querySelector('#add-card-to-hand-button');
+
+      const addCardToHandSpy = vi.spyOn(component, 'addCardToHand');
+
+      // act
+      button.click();
+
+      // assert
+      expect(addCardToHandSpy).toHaveBeenCalledOnce();
+      expect(component.handToEval().cards.length).toBe(1);
+      expect(component.handToEval().cards[0]).toEqual(cardToAdd);
+    })
   });
 });
